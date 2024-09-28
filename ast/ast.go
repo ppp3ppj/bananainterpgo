529782ae8fd6bbc0f1ef6afd776057e4cfae611f
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-28 16:11:09 +0700

    fix impl node statement

--- ast/ast.go
+++ ast/ast.go
@@ -50,5 +50,5 @@ type ReturnStatement struct {
 	ReturnVaule Expression
 }
 
-func (rs *ReturnStatement) expressionNode()      {}
+func (rs *ReturnStatement) statementNode()      {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
